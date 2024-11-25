e378d9cd7d22db7d437fa3a25498a394b3a56515
Mirzaei81 <74964876+Mirzaei81@users.noreply.github.com>
2024-11-25 13:11:09 +0330

    added Powerhsell

--- init.ps1
+++ init.ps1
@@ -1,5 +1,8 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName Microsoft.VisualBasic
+$currentPath = Get-Location
+$pipPath =Join-Path $currentPath ".venv\Scripts\pip.exe"
+$pythonPath =Join-Path  $currentPath ".venv\Scripts\python.exe"
 $title = 'Admin Username: '
 $msg   = 'Enter Admin Username: '
 
@@ -11,20 +14,17 @@ $msg   = 'Enter Admin Email: '
 $DJANGO_SUPERUSER_EMAIL= [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
 $title = 'Admin Password: '
 $msg   = 'Enter Admin Password: '
-
 $DJANGO_SUPERUSER_PASSWORD = [Microsoft.VisualBasic.Interaction]::InputBox($msg, $title)
 if(Test-Path -Path ".\.venv"){
     Write-Output ".\venv Exist"
 }else{
     python -m venv .\.venv
 } 
-.\.venv\Scripts\pip.exe install -r .\jsonSerializer\requirements.txt
-$makemigrationErrors = (.\.venv\Scripts\python.exe .\jsonSerializer\manage.py  makemigrations 2>&1 ) -join [System.Environment]::NewLine
-[System.Windows.Forms.MessageBox]::Show($makemigrationErrors, "Makemigrations ", [System.Windows.Forms.MessageBoxButtons]::OK)
-$migrationErrors=.\.venv\Scripts\python.exe .\jsonSerializer\manage.py migrate
-[System.Windows.Forms.MessageBox]::Show($migrationErrors, "Migrate", [System.Windows.Forms.MessageBoxButtons]::OK)
+Invoke-Expression "$pipPath install -r requirements.txt"
+Invoke-Expression "$pythonPath manage.py makemigrations"
+Invoke-Expression "$pythonPath manage.py migrate"
 $createSuperUserCommand  = "from django.contrib.auth.models import User; User.objects.create_superuser('$($DJANGO_SUPERUSER_USERNAME)', '$($DJANGO_SUPERUSER_EMAIL)','$($DJANGO_SUPERUSER_PASSWORD)')" 
-$createSuperUser =.\.venv\Scripts\python.exe .\jsonSerializer\manage.py shell -c $createSuperUserCommand 2>&1 | Out-String
-[System.Windows.Forms.MessageBox]::Show($runServer, "SuperUser", [System.Windows.Forms.MessageBoxButtons]::OK)
-Start-Process -FilePath ".\.venv\Scripts\python.exe" -ArgumentList @(".\jsonSerializer\manage.py", "runserver", "127.0.0.1:1234") -NoNewWindow
+Invoke-Expression "$pythonPath manage.py shell -c `"$createSuperUserCommand`"" 
+Start-Process -FilePath "$pythonPath" -ArgumentList @("manage.py", "runserver", "127.0.0.1:1234") -NoNewWindow
 Start-Process "http://127.0.0.1:1234/admin"
+Read-Host "Enter a key to close the server"
